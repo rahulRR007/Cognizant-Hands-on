e59961421864a0a27fe1e33df229190424f75d1e
rahulRR007 <118422260+rahulRR007@users.noreply.github.com>
2025-07-06 23:19:14 +0530

    Delete Difference between JPA, Hibernate and Spring Data JPA/code directory

--- Difference between JPA, Hibernate and Spring Data JPA/code/employee-data-jpa/src/main/java/com/example/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.employee.model.Employee;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
