e6b9029b24962d412070d78b0b37d3e0aff9cf42
rahulRR007 <118422260+rahulRR007@users.noreply.github.com>
2025-07-06 23:26:38 +0530

    Delete week3/week2 directory

--- week3/week2/Mocking and Stubbing/code/MockitoExample/src/main/java/com/example/MockitoExample/ExternalApi.java
+++
@@ -1,5 +0,0 @@
-package com.example.MockitoExample;
-
-public interface ExternalApi {
-	String getData();
-}
--- week3/week2/Verifying Interactions/code/MockitoVerifyExample/src/main/java/com/example/MockitoVerifyExample/ExternalApi.java
+++
@@ -1,5 +0,0 @@
-package com.example.MockitoVerifyExample;
-
-public interface ExternalApi {
-	String getData();
-}
