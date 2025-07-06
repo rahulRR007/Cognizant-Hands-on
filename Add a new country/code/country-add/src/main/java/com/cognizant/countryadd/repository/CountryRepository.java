90dcbf5ed0ac855cc471d30af0f771b47fa4fc61
rahulRR007 <118422260+rahulRR007@users.noreply.github.com>
2025-07-06 23:19:04 +0530

    Delete Add a new country/code directory

--- Add a new country/code/country-add/src/main/java/com/cognizant/countryadd/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.countryadd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.countryadd.model.Country;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-}
--- Add a new country/code/country-add/src/main/java/com/cognizant/countryadd/service/exception/CountryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.countryadd.service.exception;
-
-public class CountryNotFoundException extends Exception {
-    public CountryNotFoundException(String message) {
-        super(message);
-    }
-}
