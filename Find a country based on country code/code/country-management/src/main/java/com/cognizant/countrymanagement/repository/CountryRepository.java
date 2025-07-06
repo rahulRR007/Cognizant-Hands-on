42f67905f36fb7e768c79a34a33f29d14c411041
rahulRR007 <118422260+rahulRR007@users.noreply.github.com>
2025-07-06 23:20:38 +0530

    Delete Find a country based on country code/code directory

--- Find a country based on country code/code/country-management/src/main/java/com/cognizant/countrymanagement/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.countrymanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.countrymanagement.model.Country;
-
-public interface CountryRepository extends JpaRepository<Country, String> {
-}
